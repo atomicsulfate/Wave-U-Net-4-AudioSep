85b548a9044620ebe048291284137fbc6e8e3cad
Ahmed Ilyas <ilyas@campus.tu-berlin.de>
2021-11-06 00:46:49 +0100

    pre_commit + pytorch tutorial

---
+++ data/utils.py
@@ -0,0 +1,170 @@
+# -------------------------------------------------------------------------------
+# https://pytorch.org/tutorials/beginner/audio_preprocessing_tutorial.html
+# -------------------------------------------------------------------------------
+import io
+import os
+import math
+import tarfile
+import multiprocessing
+
+import scipy
+import librosa
+import boto3
+import requests
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+import torch
+import torchaudio
+from IPython.display import Audio, display
+
+
+def _get_sample(path, resample=None):
+    effects = [["remix", "1"]]
+    if resample:
+        effects.extend([["lowpass", f"{resample // 2}"], ["rate", f"{resample}"]])
+    return torchaudio.sox_effects.apply_effects_file(path, effects=effects)
+
+
+def get_speech_sample(*, resample=None):
+    return _get_sample(SAMPLE_WAV_SPEECH_PATH, resample=resample)
+
+
+def get_sample(*, resample=None):
+    return _get_sample(SAMPLE_WAV_PATH, resample=resample)
+
+
+def get_rir_sample(*, resample=None, processed=False):
+    rir_raw, sample_rate = _get_sample(SAMPLE_RIR_PATH, resample=resample)
+    if not processed:
+        return rir_raw, sample_rate
+    rir = rir_raw[:, int(sample_rate * 1.01) : int(sample_rate * 1.3)]
+    rir = rir / torch.norm(rir, p=2)
+    rir = torch.flip(rir, [1])
+    return rir, sample_rate
+
+
+def get_noise_sample(*, resample=None):
+    return _get_sample(SAMPLE_NOISE_PATH, resample=resample)
+
+
+def print_stats(waveform, sample_rate=None, src=None):
+    if src:
+        print("-" * 10)
+        print("Source:", src)
+        print("-" * 10)
+    if sample_rate:
+        print("Sample Rate:", sample_rate)
+    print("Shape:", tuple(waveform.shape))
+    print("Dtype:", waveform.dtype)
+    print(f" - Max:     {waveform.max().item():6.3f}")
+    print(f" - Min:     {waveform.min().item():6.3f}")
+    print(f" - Mean:    {waveform.mean().item():6.3f}")
+    print(f" - Std Dev: {waveform.std().item():6.3f}")
+    print()
+    print(waveform)
+    print()
+
+
+def get_sine_sweep(sample_rate, offset=DEFAULT_OFFSET):
+    max_sweep_rate = sample_rate
+    freq = _get_log_freq(sample_rate, max_sweep_rate, offset)
+    delta = 2 * math.pi * freq / sample_rate
+    cummulative = torch.cumsum(delta, dim=0)
+    signal = torch.sin(cummulative).unsqueeze(dim=0)
+    return signal
+
+
+def benchmark_resample(
+    method,
+    waveform,
+    sample_rate,
+    resample_rate,
+    lowpass_filter_width=DEFAULT_LOWPASS_FILTER_WIDTH,
+    rolloff=DEFAULT_ROLLOFF,
+    resampling_method=DEFAULT_RESAMPLING_METHOD,
+    beta=None,
+    librosa_type=None,
+    iters=5,
+):
+    if method == "functional":
+        begin = time.time()
+        for _ in range(iters):
+            F.resample(
+                waveform,
+                sample_rate,
+                resample_rate,
+                lowpass_filter_width=lowpass_filter_width,
+                rolloff=rolloff,
+                resampling_method=resampling_method,
+            )
+        elapsed = time.time() - begin
+        return elapsed / iters
+    elif method == "transforms":
+        resampler = T.Resample(
+            sample_rate,
+            resample_rate,
+            lowpass_filter_width=lowpass_filter_width,
+            rolloff=rolloff,
+            resampling_method=resampling_method,
+            dtype=waveform.dtype,
+        )
+        begin = time.time()
+        for _ in range(iters):
+            resampler(waveform)
+        elapsed = time.time() - begin
+        return elapsed / iters
+    elif method == "librosa":
+        waveform_np = waveform.squeeze().numpy()
+        begin = time.time()
+        for _ in range(iters):
+            librosa.resample(
+                waveform_np, sample_rate, resample_rate, res_type=librosa_type
+            )
+        elapsed = time.time() - begin
+        return elapsed / iters
+
+
+def _get_log_freq(sample_rate, max_sweep_rate, offset):
+    """Get freqs evenly spaced out in log-scale, between [0, max_sweep_rate // 2]
+
+  offset is used to avoid negative infinity `log(offset + x)`.
+
+  """
+    half = sample_rate // 2
+    start, stop = math.log(offset), math.log(offset + max_sweep_rate // 2)
+    return (
+        torch.exp(torch.linspace(start, stop, sample_rate, dtype=torch.double)) - offset
+    )
+
+
+def _get_inverse_log_freq(freq, sample_rate, offset):
+    """Find the time where the given frequency is given by _get_log_freq"""
+    half = sample_rate // 2
+    return sample_rate * (math.log(1 + freq / offset) / math.log(1 + half / offset))
+
+
+def _get_freq_ticks(sample_rate, offset, f_max):
+    # Given the original sample rate used for generating the sweep,
+    # find the x-axis value where the log-scale major frequency values fall in
+    time, freq = [], []
+    for exp in range(2, 5):
+        for v in range(1, 10):
+            f = v * 10 ** exp
+            if f < sample_rate // 2:
+                t = _get_inverse_log_freq(f, sample_rate, offset) / sample_rate
+                time.append(t)
+                freq.append(f)
+    t_max = _get_inverse_log_freq(f_max, sample_rate, offset) / sample_rate
+    time.append(t_max)
+    freq.append(f_max)
+    return time, freq
+
+
+def inspect_file(path):
+    print("-" * 10)
+    print("Source:", path)
+    print("-" * 10)
+    print(f" - File size: {os.path.getsize(path)} bytes")
+    print(f" - {torchaudio.info(path)}")
---
+++ data/viz.py
@@ -0,0 +1,179 @@
+import io
+import os
+import math
+import tarfile
+import multiprocessing
+
+import scipy
+import librosa
+import boto3
+import requests
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+import torch
+import torchaudio
+from IPython.display import Audio, display
+
+
+def plot_waveform(waveform, sample_rate, title="Waveform", xlim=None, ylim=None):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"Channel {c+1}")
+        if xlim:
+            axes[c].set_xlim(xlim)
+        if ylim:
+            axes[c].set_ylim(ylim)
+    figure.suptitle(title)
+    plt.show(block=False)
+
+
+def plot_specgram(waveform, sample_rate, title="Spectrogram", xlim=None):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].specgram(waveform[c], Fs=sample_rate)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"Channel {c+1}")
+        if xlim:
+            axes[c].set_xlim(xlim)
+    figure.suptitle(title)
+    plt.show(block=False)
+
+
+def play_audio(waveform, sample_rate):
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    if num_channels == 1:
+        display(Audio(waveform[0], rate=sample_rate))
+    elif num_channels == 2:
+        display(Audio((waveform[0], waveform[1]), rate=sample_rate))
+    else:
+        raise ValueError("Waveform with more than 2 channels are not supported.")
+
+
+def plot_spectrogram(spec, title=None, ylabel="freq_bin", aspect="auto", xmax=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Spectrogram (db)")
+    axs.set_ylabel(ylabel)
+    axs.set_xlabel("frame")
+    im = axs.imshow(librosa.power_to_db(spec), origin="lower", aspect=aspect)
+    if xmax:
+        axs.set_xlim((0, xmax))
+    fig.colorbar(im, ax=axs)
+    plt.show(block=False)
+
+
+def plot_mel_fbank(fbank, title=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title(title or "Filter bank")
+    axs.imshow(fbank, aspect="auto")
+    axs.set_ylabel("frequency bin")
+    axs.set_xlabel("mel bin")
+    plt.show(block=False)
+
+
+def get_spectrogram(n_fft=400, win_len=None, hop_len=None, power=2.0):
+    waveform, _ = get_speech_sample()
+    spectrogram = T.Spectrogram(
+        n_fft=n_fft,
+        win_length=win_len,
+        hop_length=hop_len,
+        center=True,
+        pad_mode="reflect",
+        power=power,
+    )
+    return spectrogram(waveform)
+
+
+def plot_pitch(waveform, sample_rate, pitch):
+    figure, axis = plt.subplots(1, 1)
+    axis.set_title("Pitch Feature")
+    axis.grid(True)
+
+    end_time = waveform.shape[1] / sample_rate
+    time_axis = torch.linspace(0, end_time, waveform.shape[1])
+    axis.plot(time_axis, waveform[0], linewidth=1, color="gray", alpha=0.3)
+
+    axis2 = axis.twinx()
+    time_axis = torch.linspace(0, end_time, pitch.shape[1])
+    ln2 = axis2.plot(time_axis, pitch[0], linewidth=2, label="Pitch", color="green")
+
+    axis2.legend(loc=0)
+    plt.show(block=False)
+
+
+def plot_kaldi_pitch(waveform, sample_rate, pitch, nfcc):
+    figure, axis = plt.subplots(1, 1)
+    axis.set_title("Kaldi Pitch Feature")
+    axis.grid(True)
+
+    end_time = waveform.shape[1] / sample_rate
+    time_axis = torch.linspace(0, end_time, waveform.shape[1])
+    axis.plot(time_axis, waveform[0], linewidth=1, color="gray", alpha=0.3)
+
+    time_axis = torch.linspace(0, end_time, pitch.shape[1])
+    ln1 = axis.plot(time_axis, pitch[0], linewidth=2, label="Pitch", color="green")
+    axis.set_ylim((-1.3, 1.3))
+
+    axis2 = axis.twinx()
+    time_axis = torch.linspace(0, end_time, nfcc.shape[1])
+    ln2 = axis2.plot(
+        time_axis, nfcc[0], linewidth=2, label="NFCC", color="blue", linestyle="--"
+    )
+
+    lns = ln1 + ln2
+    labels = [l.get_label() for l in lns]
+    axis.legend(lns, labels, loc=0)
+    plt.show(block=False)
+
+
+DEFAULT_OFFSET = 201
+SWEEP_MAX_SAMPLE_RATE = 48000
+DEFAULT_LOWPASS_FILTER_WIDTH = 6
+DEFAULT_ROLLOFF = 0.99
+DEFAULT_RESAMPLING_METHOD = "sinc_interpolation"
+
+
+def plot_sweep(
+    waveform,
+    sample_rate,
+    title,
+    max_sweep_rate=SWEEP_MAX_SAMPLE_RATE,
+    offset=DEFAULT_OFFSET,
+):
+    x_ticks = [100, 500, 1000, 5000, 10000, 20000, max_sweep_rate // 2]
+    y_ticks = [1000, 5000, 10000, 20000, sample_rate // 2]
+
+    time, freq = _get_freq_ticks(max_sweep_rate, offset, sample_rate // 2)
+    freq_x = [f if f in x_ticks and f <= max_sweep_rate // 2 else None for f in freq]
+    freq_y = [f for f in freq if f >= 1000 and f in y_ticks and f <= sample_rate // 2]
+
+    figure, axis = plt.subplots(1, 1)
+    axis.specgram(waveform[0].numpy(), Fs=sample_rate)
+    plt.xticks(time, freq_x)
+    plt.yticks(freq_y, freq_y)
+    axis.set_xlabel("Original Signal Frequency (Hz, log scale)")
+    axis.set_ylabel("Waveform Frequency (Hz)")
+    axis.xaxis.grid(True, alpha=0.67)
+    axis.yaxis.grid(True, alpha=0.67)
+    figure.suptitle(f"{title} (sample rate: {sample_rate} Hz)")
+    plt.show(block=True)

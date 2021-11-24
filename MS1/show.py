b7fa5d842bd43900c3bfb4a04dd70ee7a66530ac
Andres Mandado <andres.mandado-almajano@here.com>
2021-11-24 21:54:27 +0100

    Add violing plots of separation metrics to show

--- MS1/show.py
+++ MS1/show.py
@@ -11,20 +11,23 @@ from data.audio_prepro import stft
 from data.viz import plot_wave, plot_spectrogram, plot_spectrum
 from data.dataset import SeparationDataset
 from data.musdb_loader import get_musdb_folds
-
+from data.eval import create_method_store, plot_violin
+import museval
 
 
 class Plot(Flag):
     WAVE = auto()
     SPECTRUM = auto()
     SPECTROGRAM = auto()
+    TRACK_SEP_METRICS = auto()
+    GLOBAL_SEP_METRICS = auto()
 
-def _plot(plot, y, sample_rate, axes, ft=None, title_suffix=None):
+def _plot(plot, y, sample_rate, axes, metrics_store=None, ft=None, signal_name=None):
     '''
     Call the plotting function corresponding to the given plot type.
     :returns: The fourier transform of source if it was computed or given as argument.
     '''
-    suffix = f" ({title_suffix})" if title_suffix is not None else ""
+    suffix = f" ({signal_name})" if signal_name is not None else ""
     if (isinstance(y,torch.Tensor)):
         y = y.numpy().squeeze(axis=0)
     if (plot & Plot.WAVE):
@@ -36,18 +39,21 @@ def _plot(plot, y, sample_rate, axes, ft=None, title_suffix=None):
             plot_spectrogram(ft, sample_rate, axes, f"Spectrogram{suffix}")
         if (plot & Plot.SPECTRUM):
             plot_spectrum(ft, axes, f"Spectrum{suffix}")
+    #if (plot & Plot.TRACK_SEP_METRICS and metrics_store is not None):
+        # TODO
     return ft
 
-def _show_signal(source, sample_rate, axes, plots, name=None):
+def _show_signal(source, sample_rate, axes, plots, name=None, metrics_store=None):
     j = 0
     ft = None
     for plot in Plot:
         if (plots & plot):
             ax = axes[j]
             j += 1
-            ft = _plot(plot, source, sample_rate, ax, ft, name)
+            ft = _plot(plot, source, sample_rate, ax, metrics_store, ft, name)
 
-def show(x, outfile=None, names=None, y=None, plots=Plot.SPECTROGRAM | Plot.SPECTRUM | Plot.WAVE, sample_rate=22050, dpi=300):
+def show(x, outfile=None, names=None, targets=None, metrics_store=None,
+         plots=Plot.SPECTROGRAM | Plot.SPECTRUM | Plot.WAVE, sample_rate=22050, dpi=300):
     '''
     Shows multiple plots describing each of the input audio signals.
     :param x: array of audio signals to show.
@@ -57,10 +63,11 @@ def show(x, outfile=None, names=None, y=None, plots=Plot.SPECTROGRAM | Plot.SPEC
     :type outfile: str
     :param names: Optional list of names assigned to the audio signals in x (used for plot titles). By default, names
         will be 'Audio {i}' with {i} being the index of the audio signal in x.
-    :param y: If not 'None', it must be a list of the same length as x, with elements being dictionaries whose keys are
-    strings, and values are pairs (w,sr) (same meaning as x's elements). In this case, every element in x is assumed to
-    be a mixture of multiple audio sources, and its corresponding dictionary in y has the separated sources (either
-    estimations or targets).
+    :param targets: If not 'None', it must be a list of the same length as x, with elements being dictionaries whose keys are
+    strings, and values are the same type as x's elements. In this case, every element in x is assumed to
+    be a mixture of multiple audio sources, and its corresponding dictionary in targets has the separated sources.
+    :param metrics_store: Optional museval's MethodStore instance with separation performance metrics for the given dataset.
+    :type metrics_store: museval.MethodStore
     :param plots: Flags indicating the plots to show for each audio source.
     :type plots: Plot
     :param sample_rate: Sample rate for all audio signals (default 22050)
@@ -68,43 +75,51 @@ def show(x, outfile=None, names=None, y=None, plots=Plot.SPECTROGRAM | Plot.SPEC
     :param dpi: Resolution used to draw matplotlib's Figure (default 100)
     :type dpi: int
     '''
-    num_plots = 0
+
+    signal_plots = plots & ~Plot.GLOBAL_SEP_METRICS
+    num_signal_plots = 0
     for plot in Plot:
-        if (plots & plot):
-            num_plots += 1
+        if (signal_plots & plot):
+            num_signal_plots += 1
 
     plt.rc('xtick', labelsize=6)
     plt.rc('ytick', labelsize=6)
 
     num_signals = len(x)
-    if (y is not None and len(y) != num_signals):
+    if (targets is not None and len(targets) != num_signals):
         raise ValueError("y must have same length as x")
     if (names is not None and len(names) != num_signals):
         raise ValueError("names must have same length as x")
-    subfigs = np.squeeze(plt.figure(constrained_layout=True, dpi=dpi).subfigures(nrows=num_signals, ncols=1, squeeze=False), axis=1)
-
-    for signal_idx, subfig in enumerate(subfigs):
+    num_figures = num_signals if num_signal_plots > 0 else 0
+    if (plots & Plot.GLOBAL_SEP_METRICS):
+        num_figures += 1
+    subfigs = np.squeeze(plt.figure(constrained_layout=True, dpi=dpi).subfigures(nrows=num_figures, ncols=1, squeeze=False), axis=1)
+    track_figs = subfigs[:-1] if (plots & Plot.GLOBAL_SEP_METRICS) else subfigs
+    for signal_idx, subfig in enumerate(track_figs):
         signal = x[signal_idx]
         subfig_title = f'Audio {signal_idx}' if names is None else names[signal_idx]
         subfig.suptitle(subfig_title)
-        num_sources = len(y[signal_idx]) if y is not None else 0
-        axes = subfig.subplots(nrows=num_sources+1, ncols=num_plots, squeeze=False, sharex=False, sharey=False)
-        _show_signal(signal, sample_rate, axes[0],plots, None if y is None else "mix")
-        if (y is None):
+        num_targets = len(targets[signal_idx]) if targets is not None else 0
+        axes = subfig.subplots(nrows=num_targets+1, ncols=num_signal_plots, squeeze=False, sharex=False, sharey=False)
+        _show_signal(signal, sample_rate, axes[0], signal_plots, None if targets is None else "mix")
+        if (targets is None):
             continue
-        for src_idx,src in enumerate(y[signal_idx].items()):
+        for src_idx,src in enumerate(targets[signal_idx].items()):
             src_name = src[0]
             src_signal = src[1]
-            _show_signal(src_signal, sample_rate, axes[src_idx+1], plots, src_name)
+            _show_signal(src_signal, sample_rate, axes[src_idx+1], signal_plots, src_name, metrics_store)
 
+    if (plots & Plot.GLOBAL_SEP_METRICS):
+        subfig = subfigs[-1]
+        axes = subfig.subplots(1)
+        plot_violin(metrics_store, axes, methods=['ibm'], targets=list(targets[0].keys()))
     if (outfile is None):
         plt.show()
     else:
         plt.savefig(outfile)
 
-
 if __name__ == '__main__':
-    import librosa
+    #import librosa
 
     # mix = librosa.load(os.path.join(root_dir, "data/musdb/test/Al James - Schoolboy Facination.stem_mix.wav"), mono=True, sr=22050)[0]
     # vocals = librosa.load(os.path.join(root_dir,"data/musdb/test/Al James - Schoolboy Facination.stem_vocals.wav"), mono=True, sr=22050)[0]
@@ -112,7 +127,20 @@ if __name__ == '__main__':
     # show([mix], outfile=None, names=["Al James - Schoolboy Facination"], y=[{"vocals": vocals, "drums": acc}])
     #show([mix],outfile="samples.png", names=["Al James - Schoolboy Facination"], y=[{"vocals": vocals, "drums": src2}])
 
-    data = get_musdb_folds(os.path.join(root_dir,'data/musdb'), version=None)
-    test = SeparationDataset(data['test'])
-    mix, targets = test[0]
-    show([mix], outfile=None, y=[targets])
+    data = get_musdb_folds(os.path.join(root_dir,'data/musdbmini'))
+    path_list = data['train']
+    test = SeparationDataset(path_list)
+    # mix, targets = test[0]
+    # name = os.path.basename(os.path.dirname(path_list[0]['mix']))
+    # voice_inst_targets = { 'vocals': targets['vocals'], 'accompaniment': targets['accompaniment']}
+
+    mixes, targets = map(list,zip(*test))
+    targets = list(map(lambda t:  { 'vocals': t['vocals'], 'accompaniment': t['accompaniment']}, targets))
+    names =  list(map(lambda target_dict: os.path.basename(os.path.dirname(target_dict['mix'])), path_list))
+    store = create_method_store('data/musdbmini/estimates', ['ibm'])
+    store.df = store.df[store.df.track.isin(names)]
+
+    show(mixes, outfile=None, names=names, targets=targets, metrics_store=store,plots=Plot.GLOBAL_SEP_METRICS)
+
+    # show(mixes, outfile=None, names=names, targets=targets, metrics_store=store,
+    #      plots=Plot.SPECTROGRAM | Plot.SPECTRUM | Plot.WAVE | Plot.GLOBAL_SEP_METRICS)
--- data/eval.py
+++ data/eval.py
@@ -2,6 +2,7 @@ import os
 import museval
 from pathlib import Path
 import simplejson
+import numpy as np
 
 def evaluate_estimates(db, estimates_dir):
     museval.eval_mus_dir(db, estimates_dir, output_dir=estimates_dir)
@@ -26,3 +27,39 @@ def create_method_store(root_estimates_dir, method_dirs):
         store.add_evalstore(create_eval_store(method_estimates_dir), method)
 
     return store
+
+def _filter_df_col(df, col_name, col_values):
+    if (col_values is not None):
+        col_values = [col_values] if isinstance(col_values, str) else col_values
+        df = df[df[col_name].isin(col_values)]
+        if (len(col_values) == 1):
+            df = df.drop(col_name, axis=1)
+    return df
+
+def plot_violin(method_store, axes, methods=None, metrics=None, targets=None, title="Metrics"):
+    '''
+    :param method_store: Store with evaluation metrics
+    :type method_store: museval.MethodStore
+    '''
+
+    # Get median scores for each track
+    df = method_store.agg_frames_scores().reset_index(name='score')
+
+    # Get lists of scores per method-metric-target
+    df = df.groupby(['method','metric','target'])['score'].apply(list).reset_index(name='score')
+
+    # filter by provided column values
+    df = _filter_df_col( _filter_df_col( _filter_df_col(df, 'method', methods),'metric', metrics), 'target', targets)
+
+    values = df['score'].values
+    xtick_labels = df.drop('score', axis=1)
+    xlabel = ','.join(xtick_labels.columns.values)
+    xtick_labels = xtick_labels.apply(','.join, axis=1).values
+
+    axes.violinplot(values)
+    axes.set_title(title)
+    axes.set_ylabel("Score")
+    axes.set_xlabel(xlabel)
+    axes.set_xticks(np.arange(1, len(xtick_labels) + 1))
+    axes.set_xticklabels(xtick_labels)
+
